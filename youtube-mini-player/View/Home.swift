1048661238b3ede1d9dd56a1abe2f0b3586e99d2
Susumu Hoshikawa <hoshi005@me.com>
2024-02-24 16:26:36 +0900

    tabの基本実装

--- youtube-mini-player/View/Home.swift
+++ youtube-mini-player/View/Home.swift
@@ -8,8 +8,28 @@
 import SwiftUI
 
 struct Home: View {
+    @State private var activeTab: Tab = .home
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .bottom) {
+            TabView(selection: $activeTab) {
+                Text(Tab.home.rawValue)
+                    .setupTab(.home)
+                Text(Tab.shorts.rawValue)
+                    .setupTab(.shorts)
+                Text(Tab.subscriptions.rawValue)
+                    .setupTab(.subscriptions)
+                Text(Tab.you.rawValue)
+                    .setupTab(.you)
+            }
+        }
+    }
+}
+
+extension View {
+    @ViewBuilder
+    func setupTab(_ tab: Tab) -> some View {
+        self.tag(tab)
+            .toolbar(.hidden, for: .tabBar)
     }
 }
 
