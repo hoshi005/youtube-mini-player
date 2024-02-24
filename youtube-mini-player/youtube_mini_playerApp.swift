ab31585d9c79e32bca6bc49e685508047358b6b1
Susumu Hoshikawa <hoshi005@me.com>
2024-02-24 16:14:44 +0900

    first commit

---
+++ youtube-mini-player/youtube_mini_playerApp.swift
@@ -0,0 +1,17 @@
+//
+//  youtube_mini_playerApp.swift
+//  youtube-mini-player
+//
+//  Created by Susumu Hoshikawa on 2024/02/24.
+//
+
+import SwiftUI
+
+@main
+struct youtube_mini_playerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
